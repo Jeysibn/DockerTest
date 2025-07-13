c65dcdf7076cc3141dabe75595240187c5e48779
Jeysibn <jeromevillanuevaibon97@gmail.com>
2025-07-13 14:52:49 +0800

    Add Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,9 +4,9 @@ provider "aws" {
 
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
-
+  
   tags = {
-    Name        = "${var.project_name}-vpc"
+    Name        = "\-vpc"
     Environment = var.environment
   }
 }
@@ -15,21 +15,21 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidr
   map_public_ip_on_launch = true
-  availability_zone       = "${var.aws_region}a"
-
+  availability_zone       = "\a"
+  
   tags = {
-    Name        = "${var.project_name}-public-subnet"
+    Name        = "\-public-subnet"
     Environment = var.environment
   }
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0f09f9be05d8af1cd" # Amazon Linux 2023
+  ami           = "ami-0f09f9be05d8af1cd"  # Amazon Linux 2023
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public.id
-
+  
   tags = {
-    Name        = "${var.project_name}-web-server"
+    Name        = "\-web-server"
     Environment = var.environment
   }
 }
